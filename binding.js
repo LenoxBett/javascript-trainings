cb1156e64d2e176dbfef4b36e245d6aad55c0253
Lenox Bett <keringlenox@gmail.com>
2024-04-15 16:42:27 +0300

    loopstask

---
+++ binding.js
@@ -0,0 +1,12 @@
+// getting js data to html
+
+// let f_name = "Lenox"
+// let s_name = "Bett"
+// let full_name = f_name + "  " + s_name
+// document.getElementById("first").innerHTML = full_name
+
+// getting html to js
+
+let form = document.getElementById("myform")
+
+form.addEventListener("submit",function )
